8194d3fc5177b84a9e360039daf0fc3b1505996b
bondyuu <leo4902@gmail.com>
2025-04-22 10:04:51 +0900

    back setting

---
+++ back/src/main/java/com/bondyu/toy_chat/ToyChatApplication.java
@@ -0,0 +1,13 @@
+package com.bondyu.toy_chat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToyChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToyChatApplication.class, args);
+	}
+
+}
---
+++ back/src/test/java/com/bondyu/toy_chat/ToyChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bondyu.toy_chat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToyChatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
